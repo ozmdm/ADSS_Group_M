d4e313c776f5a8c26ede0711af1cd335a0ed1dbd
kimpatz <perezdo@post.bgu.ac.il>
2020-04-20 16:56:50 +0300

    add some Tests

--- dev/src/Test/TestClass.java
+++ dev/src/Test/TestClass.java
@@ -1,4 +1,5 @@
 package Test;
+
 import Data.Data;
 import bussinessLayer.Supplier;
 import org.junit.jupiter.api.Test;
@@ -7,63 +8,93 @@ import ServiceLayer.OrderService;
 import ServiceLayer.SupplierService;
 import bussinessLayer.Order;
 
-import  static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestClass {
-	
-	private static SupplierService supService = SupplierService.getInstance();
-	private static OrderService oService = OrderService.getInstance();
-	
-	
-	@BeforeAll
-	public static void setup() {
-		supService.loadFirstSuppliers();
-		oService.loadFirstItems();
-	}
-	
+
+    private static SupplierService supService = SupplierService.getInstance();
+    private static OrderService oService = OrderService.getInstance();
+
+
+    @BeforeAll
+    public static void setup() {
+        supService.loadFirstSuppliers();
+        oService.loadFirstItems();
+    }
 
     @Test
-    public void CheckAddingItemsToCatalogItems(){
+    public void createOrder() {
+        List<Order> orders = Data.getOrders();
+        int ordersSize = orders.size();
+        oService.createAnOrder(1);
+        assertEquals(ordersSize + 1, orders.size(), "Size of orders wrong");
     }
-    
+
     @Test
-    public void createOrder(){
-    	List<Order> orders = Data.getOrders();
-    	int ordersSize = orders.size();
-    	oService.createAnOrder(1);
-    	assertEquals(ordersSize+1, orders.size(), "Size of orders wrong");
+    public void creatSupplier() {
+        List<Supplier> suppliers = Data.getSuppliers();
+        int suppliersSize = suppliers.size();
+        supService.AddSupplier("d", 0, 1, "EOM30", true);
+        assertEquals(suppliersSize + 1, suppliers.size(), "Size of suppliers wrong");
     }
+
     @Test
-    public void creatSupplier(){
-	    List<Supplier> suppliers = Data.getSuppliers();
-	    int suppliersSize = suppliers.size();
-	    supService.AddSupplier("d",0,1,"EOM30",true);
-	    assertEquals(suppliersSize+1,suppliers.size(),"Size of suppliers wrong");
-    }
-    @Test
-    public void c(){
-	    List<Supplier> suppliers = Data.getSuppliers();
-        supService.AddSupplier("d",0,1,"EOM30",true);
+    public void addItemToCatalog() {
+        List<Supplier> suppliers = Data.getSuppliers();
+        supService.AddSupplier("d", 0, 1, "EOM30", true);
         int catalogSize = supService.getSupplierById(0).getContract().getCatalog().getItems().size();
-        supService.addCatalogItemToCatalogInContract(0,1,10,6);
-        assertEquals(catalogSize+1,supService.getSupplierById(0).getContract().getCatalog().getItems().size(),"Size of catalogItem wrong");
+        supService.addCatalogItemToCatalogInContract(0, 1, 10, 6);
+        assertEquals(catalogSize + 1, supService.getSupplierById(0).getContract().getCatalog().getItems().size(), "Size of catalogItem wrong");
     }
+
     @Test
-    public void d(){
+    public void RemoveItemFromCatalog() {
+        addItemToCatalog();
+        int catalogSize = supService.getSupplierById(0).getContract().getCatalog().getItems().size();
+        supService.deleteCatalogItemFromCatlogInContract(0, 10);
+        assertEquals(catalogSize - 1, supService.getSupplierById(0).getContract().getCatalog().getItems().size(), "Size of catalogItem wrong");
     }
+
     @Test
-    public void e(){
+    public void IsExist() {
+        List<Supplier> suppliers = Data.getSuppliers();
+        int suppliersSize = suppliers.size();
+        supService.AddSupplier("d", 0, 1, "EOM30", true);
+        assertTrue(supService.isExist(0), "dont exist problem with the method");
+        supService.removeSupplier(0);
+        assertFalse(supService.isExist(0), "supplier exist problem with the method");
+
     }
+
     @Test
-    public void f(){
+    public void removeSupplier() {
+        List<Supplier> suppliers = Data.getSuppliers();
+        int suppliersSize = suppliers.size();
+        supService.AddSupplier("d", 0, 1, "EOM30", true);
+         suppliersSize = suppliers.size();
+        supService.removeSupplier(0);
+        suppliers = Data.getSuppliers();
+        assertEquals(suppliersSize - 1, suppliers.size(), "Size of suppliers wrong");
     }
+
     @Test
-    public void g(){
+    public void g() {
+    }
+
+    @Test
+    public void h() {
+    }
+
+    @Test
+    public void i() {
+    }
+
+    @Test
+    public void j() {
     }
 }
