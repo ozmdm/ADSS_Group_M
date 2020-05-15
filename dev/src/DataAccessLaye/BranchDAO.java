64080b19859dc845f530ee9a57f3cc1ca87618ef
Dor7777 <perezdo@post.bgu.ac.il>
2020-05-15 03:15:16 +0300

    update Data access layer

--- dev/src/DataAccessLaye/BranchDAO.java
+++ dev/src/DataAccessLaye/BranchDAO.java
@@ -1,4 +1,12 @@
 package DataAccessLaye;
 
+import java.sql.Connection;
+
 public class BranchDAO {
+
+    private Connection conn;
+    public BranchDAO(Connection conn)
+    {
+        this.conn = conn;
+    }
 }
--- dev/src/DataAccessLaye/Repo.java
+++ dev/src/DataAccessLaye/Repo.java
@@ -9,19 +9,63 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Repo {
+    public static Repo repo;
     private Connection con;
     private BranchDAO branchDAO;
-    private CatalogItemDAOImpl catalogItemDAO;
-    private ContactDaoImpl contactDao;
+    private ICatalogItemDAO catalogItemDAO;
+    private IContactDAO contactDao;
     private DamagedControllerDAO damagedControllerDAO;
-    private DeliveryDaysDAOImpl deliveryDaysDAO;
+    private IDeliveryDaysDAO deliveryDaysDAO;
     private InventoryDAO inventoryDAO;
     private ItemDAO itemDAO;
     private ItemStatusDAO itemStatusDAO;
-    private LineCatalogItemInCartDAOImpl lineCatalogItemInCartDAO;
-    private OrderDAOImpl orderDAO;
-    private RangesDAODAOImpl rangesDAODAO;
-    private ScheduledDAOImpl scheduledDAO;
-    private SupplierDAOImpl supplierDAO;
+    private ILineCatalogItemInCartDAO lineCatalogItemInCartDAO;
+    private IOrderDAO orderDAO;
+    private IRangesDAO rangesDAODAO;
+    private IScheduledOrderDAO scheduledDAO;
+    private ISupplierDAO supplierDAO;
 
+    private Repo() throws Exception {
+         String url = "jdbc:sqlite:C://sqlite/db/test.db";
+         con = DriverManager.getConnection(url);
+        branchDAO = new BranchDAO(con);
+        catalogItemDAO = new CatalogItemDAOImpl(con);
+        contactDao = new ContactDaoImpl(con);
+        damagedControllerDAO = new DamagedControllerDAO(con);
+        deliveryDaysDAO = new DeliveryDaysDAOImpl(con);
+        inventoryDAO = new InventoryDAO(con);
+        itemDAO = new ItemDAO(con);
+        itemStatusDAO = new ItemStatusDAO(con);
+        lineCatalogItemInCartDAO = new LineCatalogItemInCartDAOImpl(con);
+        orderDAO = new OrderDAOImpl(con);
+        rangesDAODAO = new RangesDAODAOImpl(con);
+        scheduledDAO = new ScheduledDAOImpl(con);
+        supplierDAO = new SupplierDAOImpl(con);
+    }
+
+    public static Repo getInstance() throws Exception {
+        if (repo!=null) return repo;
+        repo = new Repo();
+        return repo;
+    }
+
+    public void creatTable() throws SQLException {
+
+        String sqlQ = "CREATE TABLE IF NOT EXISTS Supplier (\n"
+                + "	name varchar(50),\n"
+                + "	supplierId INTEGER NOT NULL,\n"
+                + "	bankAccountNumber INTEGER \n"
+                +"CONSTRAINT PK_Supplier Primary KEY(supplierId) \n"
+                +");";
+        sqlQ += "CREATE TABLE IF NOT EXISTS Supplier (\n"
+                + "	name varchar(50),\n"
+                + "	supplierId INTEGER NOT NULL,\n"
+                + "	bankAccountNumber INTEGER \n"
+                +"CONSTRAINT PK_Supplier Primary KEY(supplierId) \n"
+                +");";
+
+        Statement stmt = con.createStatement();
+        stmt.execute(sqlQ);
+
+    }
 }
