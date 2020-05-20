b5071f1f30b97c9f3f4990b6236cd37cec39ddbd
Niv Davidian <nivdavid@post.bgu.ac.il>
2020-05-20 23:54:50 +0300

    niv

--- dev/src/PresentationLayer/MainUserInterface.java
+++ dev/src/PresentationLayer/MainUserInterface.java
@@ -293,7 +293,7 @@ public class MainUserInterface {
         			printOpenOrders(branchId);
         			break;
         		case 10:
-        			
+        			break;
         		case 11:
         			break;
                 default:
